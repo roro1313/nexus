7d737965d43b5f1bd9c54c89cac4cfac1858e8d9
roro1313 <rociorgz@gmail.com>
2021-07-12 21:21:06 +0200

    todas las rutas hechas y funcionando, falta añadir isAutenticated( )

--- server/index.js
+++ server/index.js
@@ -5,9 +5,7 @@ let puerto = process.env.PORT || 3000;
 
 const mongodb = require("mongodb");
 let MongoClient = mongodb.MongoClient;
-/* let db; */
-
-const users = require("./users")
+let db;
 
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
@@ -21,14 +19,15 @@ app.use(
     secret: ["patata", "zanahoria", "rabanos", "secret"],
     resave: false,
     saveUninitialized: false,
-    // cookie: { maxAge: 600000 }
   })
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use("/users", users);
+/* ------------------------------------------------------------------------------------------ */
+/* ------------------------------------ CONEXIÓN MONGODB ------------------------------------ */
+/* ------------------------------------------------------------------------------------------ */
 
 MongoClient.connect(
   process.env.MONGO_URL,
@@ -36,11 +35,12 @@ MongoClient.connect(
   function (error, client) {
     error
       ? console.log("🔴 MongoDb no está conectado")
-      : ((app.locals.db = client.db("empresa")), console.log("🟢 MongoDb conectado"));
+      : ((db = client.db("empresa")), console.log("🟢 MongoDb conectado"));
   }
 );
-
-/* ------ USO DE SESIONES ------ */
+/* ----------------------------------------------------------------------------------------- */
+/* ------------------------------------ USO DE SESIONES ------------------------------------ */
+/* ----------------------------------------------------------------------------------------- */
 
 passport.use(
   new LocalStrategy(
@@ -65,12 +65,12 @@ passport.use(
   )
 );
 
-passport.serializeUser(function (user, done) {
+passport.serializeUser((user, done) => {
   done(null, user.email);
 });
 
-passport.deserializeUser(function (email, done) {
-  db.collection("users").findOne({ email: email }, function (err, user) {
+passport.deserializeUser((email, done) => {
+  db.collection("users").findOne({ email: email }, (err, user) => {
     if (err) {
       return done(err);
     }
@@ -81,7 +81,9 @@ passport.deserializeUser(function (email, done) {
   });
 });
 
-/* ------ LOGIN ------ */
+/* ------------------------------------------------------------------------------- */
+/* ------------------------------------ LOGIN ------------------------------------ */
+/* ------------------------------------------------------------------------------- */
 
 app.post(
   "/login",
@@ -91,25 +93,60 @@ app.post(
   })
 );
 
-app.all("/api", function (req, res) {
+app.all("/api", (req, res) => {
   res.send({ logged: true, mensaje: "Login correcto" });
 });
 
-app.all("/api/failed", function (req, res) {
+app.all("/api/failed", (req, res) => {
   res.status(401).send({ logged: false, mensaje: "Denegado" });
 });
 
-/* ------ LOGOUT ------ */
+/* -------------------------------------------------------------------------------- */
+/* ------------------------------------ LOGOUT ------------------------------------ */
+/* -------------------------------------------------------------------------------- */
 
-app.post("/logout", function (req, res) {
+app.post("/logout", (req, res) => {
   req.logOut();
   res.send({ mensaje: "Sesión cerrada correctamente" });
 });
 
-/* ------ RUTAS ------ */
+/* ---------------------------------------------------------------------------------------- */
+/* ------------------------------------ RUTA DE PRUEBA ------------------------------------ */
+/* ---------------------------------------------------------------------------------------- */
 
-/* app.post("/signup", function (req, res) {
-  //Esta ruta será /admin/create
+app.get("/prueba", (req, res) => {
+  req.isAuthenticated()
+    ? res.send({ mensaje: "La sesión está iniciada" })
+    : res.send({ mensaje: "Sesión no iniciada" });
+});
+
+/* --------------------------------------------------------------------------------------------- */
+/* ------------------------------------ RUTAS ADMINISTRADOR ------------------------------------ */
+/* --------------------------------------------------------------------------------------------- */
+
+//------------------ Rutas de creación, edición y borrado de datos
+
+app.get("/admin", (req, res) => {
+  if (req.isAuthenticated()) {
+    db.collection("users")
+      .find({
+        email: req.body.email,
+        password: req.body.password,
+      })
+      .toArray(function (error, datos) {
+        error
+          ? res.send({ error: true, respuesta: error })
+          : res.send({ error: false, respuesta: datos });
+      });
+  } else {
+    res.send({
+      mensaje:
+        "No se puede acceder a los datos de administrador sin iniciar sesión",
+    });
+  }
+});
+
+app.post("/admin/create", (req, res) => {
   // Aqui es recomendable añadir la encriptación del password con BCrypt
   db.collection("users")
     .find({ email: req.body.email })
@@ -131,7 +168,7 @@ app.post("/logout", function (req, res) {
             vacaciones: req.body.vacaciones,
             docs: req.body.docs,
           },
-          function (err, respuesta) {
+          function (err, resp) {
             if (err !== null) {
               console.log(err);
               res.send({ mensaje: "Ha habido un error: " + err });
@@ -144,15 +181,311 @@ app.post("/logout", function (req, res) {
         res.send({ mensaje: "Usuario ya registrado" });
       }
     });
-}); */
-
-app.get("/prueba", function (req, res) {
-  req.isAuthenticated()
-    ? res.send({ mensaje: "La sesión está iniciada" })
-    : res.send({ mensaje: "Sesión no iniciada" });
 });
 
-/* app.get("/user", function (req, res) {
+app.put("/admin/edit", (req, res) => {
+  db.collection("users").updateOne(
+    { email: req.body.email },
+    {
+      $set: {
+        nombre: req.body.nombre,
+        apellido: req.body.apellido,
+        foto: req.body.foto,
+        puesto: req.body.puesto,
+        departamento: req.body.departamento,
+        sede: req.body.sede,
+        movil: req.body.movil,
+        meeting: req.body.meeting,
+        training: req.body.training,
+        vacaciones: req.body.vacaciones,
+        docs: req.body.docs,
+      },
+    },
+    function (error, datos) {
+      error
+        ? res.send({ error: true, contenido: error })
+        : res.send({
+            error: false,
+            mensaje: `Se ha modificado ${datos.modifiedCount} registro correctamente`,
+            contenido: datos,
+          });
+    }
+  );
+});
+
+app.delete("/admin/delete", (req, res) => {
+  db.collection("users").deleteOne(
+    { email: req.body.email },
+    {
+      $set: {
+        nombre: req.body.nombre,
+        apellido: req.body.apellido,
+        foto: req.body.foto,
+        puesto: req.body.puesto,
+        departamento: req.body.departamento,
+        sede: req.body.sede,
+        movil: req.body.movil,
+        meeting: req.body.meeting,
+        training: req.body.training,
+        vacaciones: req.body.vacaciones,
+        docs: req.body.docs,
+      },
+    },
+    function (error, datos) {
+      error
+        ? res.send({ error: true, contenido: error })
+        : res.send({
+            error: false,
+            mensaje: `Se ha eliminado ${datos.deletedCount} registro correctamente`,
+            contenido: datos,
+          });
+    }
+  );
+});
+
+//------------------ Rutas de consulta de información
+
+app.get("/admin/company", (req, res) => {
+  db.collection("users")
+    .find()
+    .toArray((error, datos) => {
+      error
+        ? res.send({ error: true, respuesta: error })
+        : res.send({ error: false, respuesta: datos });
+    });
+});
+
+app.post("/admin/user", (req, res) => {
+  db.collection("users")
+    .find({ email: req.body.email })
+    .toArray((error, datos) => {
+      error
+        ? res.send({ error: true, respuesta: error })
+        : res.send({ error: false, respuesta: datos });
+    });
+});
+
+app.post("/admin/departamento", (req, res) => {
+  db.collection("users")
+    .find({ departamento: req.body.departamento })
+    .toArray((error, datos) => {
+      error
+        ? res.send({ error: true, respuesta: error })
+        : res.send({ error: false, respuesta: datos });
+    });
+});
+
+app.post("/admin/sede", (req, res) => {
+  db.collection("users")
+    .find({ sede: req.body.sede })
+    .toArray((error, datos) => {
+      error
+        ? res.send({ error: true, respuesta: error })
+        : res.send({ error: false, respuesta: datos });
+    });
+});
+
+//------------------ Rutas de administración de formaciones:
+
+app.get("/admin/training", (req, res) => {
+  db.collection("training")
+    .find()
+    .toArray((error, datos) => {
+      error
+        ? res.send({ error: true, respuesta: error })
+        : res.send({ error: false, respuesta: datos });
+    });
+});
+
+app.post("/admin/training/create", (req, res) => {
+  db.collection("training")
+    .find({ code: req.body.code })
+    .toArray((error, data) => {
+      if (error) {
+        res.send({ error: true, contenido: error });
+      } else {
+        if (data.length === 1) {
+          res.send({
+            error: false,
+            mensaje: "El código de formación ya existe",
+          });
+        } else {
+          db.collection("training").insertOne(
+            {
+              nombre: req.body.nombre,
+              code: req.body.code,
+              descripcion: req.body.descripcion,
+              fecha: req.body.fecha,
+              lugar: req.body.lugar,
+              asistentes: req.body.asistentes,
+            },
+            function (error, datos) {
+              error
+                ? res.send({ error: true, contenido: error })
+                : res.send({
+                    error: false,
+                    mensaje: `Se ha creado ${datos.insertedCount} formación correctamente`,
+                    contenido: datos,
+                  });
+            }
+          );
+        }
+      }
+    });
+});
+
+app.put("/admin/training/edit", (req, res) => {
+  db.collection("training").updateOne(
+    { code: req.body.code },
+    {
+      $set: {
+        nombre: req.body.nombre,
+        descripcion: req.body.descripcion,
+        fecha: req.body.fecha,
+        lugar: req.body.lugar,
+        asistentes: req.body.asistentes,
+      },
+    },
+    function (error, datos) {
+      error
+        ? res.send({ error: true, contenido: error })
+        : res.send({
+            error: false,
+            mensaje: `Se ha modificado ${datos.modifiedCount} formación correctamente`,
+            contenido: datos,
+          });
+    }
+  );
+});
+
+app.delete("/admin/training/delete", (req, res) => {
+  db.collection("training").deleteOne(
+    { code: req.body.code },
+    {
+      $set: {
+        nombre: req.body.nombre,
+        descripcion: req.body.descripcion,
+        fecha: req.body.fecha,
+        lugar: req.body.lugar,
+        asistentes: req.body.asistentes,
+      },
+    },
+    function (error, datos) {
+      error
+        ? res.send({ error: true, contenido: error })
+        : res.send({
+            error: false,
+            mensaje: `Se ha eliminado ${datos.deletedCount} formación correctamente`,
+            contenido: datos,
+          });
+    }
+  );
+});
+
+//------------------ Rutas de administración de eventos:
+
+app.get("/admin/meeting", (req, res) => {
+  db.collection("meeting")
+    .find()
+    .toArray((error, datos) => {
+      error
+        ? res.send({ error: true, respuesta: error })
+        : res.send({ error: false, respuesta: datos });
+    });
+});
+
+app.post("/admin/meeting/create", (req, res) => {
+  db.collection("meeting")
+    .find({ code: req.body.code })
+    .toArray((error, data) => {
+      if (error) {
+        res.send({ error: true, contenido: error });
+      } else {
+        if (data.length === 1) {
+          res.send({
+            error: false,
+            mensaje: "El código de reunión ya existe",
+          });
+        } else {
+          db.collection("meeting").insertOne(
+            {
+              nombre: req.body.nombre,
+              code: req.body.code,
+              descripcion: req.body.descripcion,
+              fecha: req.body.fecha,
+              lugar: req.body.lugar,
+              asistentes: req.body.asistentes,
+            },
+            function (error, datos) {
+              error
+                ? res.send({ error: true, contenido: error })
+                : res.send({
+                    error: false,
+                    mensaje: `Se ha creado ${datos.insertedCount} reunión correctamente`,
+                    contenido: datos,
+                  });
+            }
+          );
+        }
+      }
+    });
+});
+
+app.put("/admin/meeting/edit", (req, res) => {
+  db.collection("meeting").updateOne(
+    { code: req.body.code },
+    {
+      $set: {
+        nombre: req.body.nombre,
+        descripcion: req.body.descripcion,
+        fecha: req.body.fecha,
+        lugar: req.body.lugar,
+        asistentes: req.body.asistentes,
+      },
+    },
+    function (error, datos) {
+      error
+        ? res.send({ error: true, contenido: error })
+        : res.send({
+            error: false,
+            mensaje: `Se ha modificado ${datos.modifiedCount} reunión correctamente`,
+            contenido: datos,
+          });
+    }
+  );
+});
+
+app.delete("/admin/meeting/delete", (req, res) => {
+  db.collection("meeting").deleteOne(
+    { code: req.body.code },
+    {
+      $set: {
+        nombre: req.body.nombre,
+        descripcion: req.body.descripcion,
+        fecha: req.body.fecha,
+        lugar: req.body.lugar,
+        asistentes: req.body.asistentes,
+      },
+    },
+    function (error, datos) {
+      error
+        ? res.send({ error: true, contenido: error })
+        : res.send({
+            error: false,
+            mensaje: `Se ha eliminado ${datos.deletedCount} reunión correctamente`,
+            contenido: datos,
+          });
+    }
+  );
+});
+
+//------------------ Rutas de administración de vacaciones:
+
+/* --------------------------------------------------------------------------------------------------- */
+/* ------------------------------------------ RUTAS USUARIO ------------------------------------------ */
+/* --------------------------------------------------------------------------------------------------- */
+
+app.get("/user", (req, res) => {
   if (req.isAuthenticated()) {
     db.collection("users")
       .find({
@@ -165,9 +498,11 @@ app.get("/prueba", function (req, res) {
           : res.send({ error: false, respuesta: datos });
       });
   } else {
-    res.send({ mensaje: "No se puede acceder a los datos de usuario sin iniciar sesión" });
+    res.send({
+      mensaje: "No se puede acceder a los datos de usuario sin iniciar sesión",
+    });
   }
-}); */
+});
 
 app.listen(puerto, (error) => {
   error
