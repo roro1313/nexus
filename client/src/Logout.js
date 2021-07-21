75acc42dcdb098c32fe2f04c51ac3e5fe60943ae
roro1313 <rociorgz@gmail.com>
2021-07-21 12:37:38 +0200

    añadido logout funcionando

---
+++ client/src/Logout.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+
+function Logout(props) {
+  let [logout, setLogout] = useState({});
+
+  useEffect(() => {
+    fetch("http://localhost:3001/logout", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: props.loginData,
+      }),
+      credentials: "include",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setLogout(data);
+        props.setLoginData("");
+      });
+  }, []);
+
+  return (
+    <>
+      <Container>
+        <Row>
+        <h4>{logout.mensaje}</h4>
+        <p>Recuerda que puedes volver a iniciar sesión en cualquier momento para ver y editar tus datos.</p>
+        </Row>
+      </Container>
+    </>
+  );
+}
+
+export default Logout;
