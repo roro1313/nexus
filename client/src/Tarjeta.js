8cdbe8be8ee48f801faefa6ed8bd3e802a1aa537
roro1313 <rociorgz@gmail.com>
2021-07-23 09:39:54 +0200

    fix resultados búsqueda users

--- client/src/Tarjeta.js
+++ client/src/Tarjeta.js
@@ -3,24 +3,25 @@ import Row from "react-bootstrap/Row";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
-import { Redirect } from "react-router-dom";
+import { Redirect, Link } from "react-router-dom";
 
 function Tarjeta(props) {
   const enlace = () => {
-    return <Redirect to="/admin/edit" />;
+    return <Link to="/admin/edit" />;
   };
 
   const usuarios = props.resultadoBusqueda.map((user, index) => {
     return (
-      <Card key={index}>
+      <Card className={"usuarios"} key={index} style={{ width: "13rem" }}>
         <Card.Img variant="top" src={user.foto} />
         <Card.Body>
-          <Card.Title>{user.nombre}</Card.Title>
-          <Card.Title>{user.apellido}</Card.Title>
-          <Card.Text>
-            {user.puesto} {user.departamento}
-          </Card.Text>
-          <Card.Text></Card.Text>
+          <Card.Title>
+            {user.nombre} {user.apellido}
+          </Card.Title>
+          <Card.Title>{user.puesto}</Card.Title>
+          <Card.Text>{user.departamento}</Card.Text>
+          <Card.Text>{user.email}</Card.Text>
+          <Card.Text>{user.movil}</Card.Text>
           {/* <Button onClick={enlace}>Editar usuario</Button> */}
         </Card.Body>
       </Card>
